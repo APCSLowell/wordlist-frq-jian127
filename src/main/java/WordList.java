00b45a956b44baf084a3958d0ed700a17252f94b
jian127 <142540574+jian127@users.noreply.github.com>
2024-04-05 12:34:36 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,23 +11,21 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     int count = 0;
-   for(int i = 0; i < myList.size(); i ++){
-     if(myList.get(i).length() ==  len){
+   for(int i = 0; i < myList.size(); i ++)
+     if(myList.get(i).length() ==  len)
        count++;
-     }
-   }
+   
     return count;
     
   }
 
   public void removeWordsOfLength(int len)
   {
-    for(int c = 0; c < myList.size(); c++){
-      if(myList.get(c).length() == len{
+    for(int c = 0; c < myList.size(); c++)
+      if(myList.get(c).length() == len
         myList.remove(c);
         c--;
-      }
-    } 
+      
+    }
 
   }
-}
